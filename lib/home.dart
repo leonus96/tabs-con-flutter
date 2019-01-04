6b49d735aeca85b34094fc010c1e7c71ab6e1276
Joseph León <leonus96@gmail.com>
2019-01-04 10:52:38 -0500

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {}
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Pestañas con FLutter'),
+          backgroundColor: Colors.blue,
+        ),
+      );
+    }
+}
