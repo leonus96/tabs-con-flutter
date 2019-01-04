9ae938b4f11c6f44b3ff4a673339e341ab3edc1e
Joseph León <leonus96@gmail.com>
2019-01-04 11:32:37 -0500

    segundo video

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,21 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+    void initState() {
+      super.initState();
+      _controller = TabController(length: 4, vsync: this);
+    }
+  
+  @override
+    void dispose() {
+      _controller.dispose();
+      super.dispose();
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,4 +29,32 @@ class _HomeState extends State<Home> {
         body: getTabBarView(),
       );
   }
+
+  TabBar getTabBar() {
+    return TabBar(
+      tabs: <Tab>[
+        Tab(
+          icon: Icon(Icons.looks_one),
+        ),
+        Tab(
+          icon: Icon(Icons.looks_two),
+        ),
+        Tab(
+          icon: Icon(Icons.looks_3),
+        ),
+        Tab(
+          icon: Icon(Icons.looks_4),
+        ),
+      ],
+      controller: _controller,
+    );
+  }
+
+  TabBarView getTabBarView(var tabs) {
+    return TabBarView(
+      children: tabs,
+      controller: _controller,
+    );
+  }
+
 }
